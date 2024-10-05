00ac28ab9676240ca6d6981fd8114bd1cd659835
sudipto <cg.sudipc@gmail.com>
2024-10-05 10:33:34 +0530

    reverse array, 2nd largest in array

--- src/com/reaplly/progrmming/StringAndArrayPgms.java
+++ src/com/reaplly/progrmming/StringAndArrayPgms.java
@@ -1,31 +1,36 @@
 package com.reaplly.progrmming;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 
 public class StringAndArrayPgms {
 	/**
 	 * Common String problems
-	 * 0. find duplicate characters
-	 * 1. reverse a string 						- done
-	 * 2. check if a string is a palindrome 	- done
-	 * 3. find first non-repeating character	-
-	 * 4. count occurances of a character in a string
-	 * 5. check if two strings are anagrams - done
-	 * 6. longest substring without repeating characters
-	 * 7. check if a string only contains digits
-	 * 8. string compression ->  Input: "aaabbccc", Output: "a3b2c3"
-	 * 9. find all permutations of string
-	 * 10.find the most frequent character in a string
+	 * 
+	 * - *reverse a string 						- done	[2 pointer]
+	 * - *check if two strings are anagrams 	- done	[sort, compare]
+	 * - *find all occurances of substring
+	 * - *sort string of chars 
+	 * - *count unique chars in a string
+	 * - find first duplicate character			- done 	[map]
+	 * - count occurances/duplicate chars		- done	[map]
+	 * - check if a string is a palindrome 		- done	[2 pointer]
+	 * - find first non-repeating character		- done 	[LinkedHasMmap]
+	 * - longest substring without repeating chars	
+	 * - find the most frequent character in a string
 	 * */
 	
 	
-	// count duplicate character
-	public static char findDuplicates(String s) {
+	// find first duplicate character
+	public static char findFirstDuplicate(String s) {
 		Map<Character, Integer> cmap = new HashMap<>();
 		for(char c : s.toCharArray()) {
 			if(cmap.containsKey(c)) {
@@ -36,6 +41,25 @@ public class StringAndArrayPgms {
 		}
 		return Character.MIN_VALUE;
 	}
+	
+	// find duplicate chars
+	public static List<Character> findDuplicateChars(String s){
+		Map<Character, Integer> cmap = new HashMap<>();
+		for(char c : s.toCharArray()) {
+			if(cmap.containsKey(c)) {
+				cmap.put(c, cmap.get(c) + 1);
+			}else {
+				cmap.put(c, 1);
+			}
+		}
+		List<Character> list = new ArrayList<>();
+		for(Map.Entry<Character, Integer> entry: cmap.entrySet()) {
+			if(entry.getValue() > 1) {
+				list.add(entry.getKey());
+			}
+		}
+		return list;
+	} 
 	
 	//reverse a string
 	public static String reverseString1(String str) {
@@ -114,14 +138,95 @@ public class StringAndArrayPgms {
 			return carr;
 		}
 	
-	// find first non repeating character in a string
-//	public static char firstRepeatingChar(String s) {
-//		for
-//	}
+	// find first non repeating character in a string // swiss
+	public static char firstNonRepeatedChar(String s) {
+		Map<Character, Integer> map = new LinkedHashMap<>();
+		for(char c : s.toCharArray()) {
+			if(map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			}else {
+				map.put(c, 1);
+			}
+		}
+		for(Map.Entry<Character, Integer> entry : map.entrySet()) {
+			if(entry.getValue() == 1) {
+				return entry.getKey();
+			}
+		}
+		return Character.MIN_VALUE;
+	}
+	
+	
+	
+	/**
+	 * Array problems
+	 * 
+	 * - Reverse an array of integers.
+	 * - find the second largest element in array
+	 * - Rotate array to right by k steps
+	 * - Remove duplicates from sorted array
+	 * - binary search
+	 * - swap two arrays
+	 * - Reverse a linked list
+	 * - Detect cycle in linked list
+	 * - Merge sorted linked list
+	 * - Implement stack queue using array
+	 * - check if parenthesis in string are balanced
+	 * */
+	
+	// Reverse an array of integers.
+	public static int[] reverseArray(int[] nums) {
+		if(nums.length == 0 || nums.length == 1) {
+			return nums;
+		}
+		int p1 = 0;
+		int p2 = nums.length-1;
+		while(p1<=p2) {
+			int temp = nums[p1];
+			nums[p1] = nums[p2];
+			nums[p2] = temp;
+			p1++;
+			p2--;
+		}
+		return nums;
+	}
+	
+	public static int secondLargest(int[] nums) {
+		if(nums.length == 1) {
+			return nums[0];
+		}
+		if(nums.length == 0) {
+			return -1;
+		}
+		Set<Integer> set = new TreeSet<>();
+		for(int i : nums) {
+			set.add(i);
+		}
+		int[] arr = new int[set.size()];
+		int k = 0;
+		for(Integer i : set) {
+			arr[k] = i;
+			k++;
+		}
+		
+		return arr[arr.length-2];
+	}
+	
+	
+	/**
+	 * Math problems
+	 * 
+	 * - hcf, lcm
+	 * - sum of prime numbers between 1 and N
+	 * 
+	 * */
+	
+	
+	
 		
 	public static void main(String[] args) {
-		String s1 = "abcabc";
-		System.out.println(findDuplicates(s1));
+		int[] nums = {};
+		System.out.println(secondLargest(nums));
 	}
 
 }
